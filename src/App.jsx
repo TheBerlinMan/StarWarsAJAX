d3e7e6680d45d154d31d6c35d17756e603fa5551
Berlin <tommyonik@gmail.com>
2024-02-05 17:17:39 -0500

    connect api and set api in state

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,28 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+//npm modules
+import { useState, useEffect } from 'react'
+import { Link } from 'react-router-dom'
+
+//css
 import './App.css'
 
+//services
+import { getAllStarships } from '../services/sw-api'
+
 function App() {
-  const [count, setCount] = useState(0)
+  
+  const[starshipList, setStarshipList] = useState([])
+
+  useEffect(() =>{
+    const fetchStarshipList = async () => {
+      const starshipData = await getAllStarships()
+      setStarshipList(starshipData)
+    }
+    fetchStarshipList
+  }, [])
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+
     </>
   )
 }
